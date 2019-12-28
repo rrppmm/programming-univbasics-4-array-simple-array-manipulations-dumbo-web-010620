6fc14e31b4e2fea4410a3f00a945994daac26e9b
Paul M <paulmcadory@gmail.com>
2019-12-28 18:52:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,8 +23,8 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-  array1 = []
-  array2 = []
+  array1 = ["raindrops on roses", "whiskers on kittens"]
+  array2 = ["sports cars", "flatiron school"]
   array1.concat(array1, array2)
 end
 
